fdfdf4892a4e4e2b7f97bcea2455a0e8515a8721
Christian Nagel <christian@christiannagel.com>
2017-06-07 11:43:22 +0200

    C# 7.0 samples

---
+++ CSharp7Samples/CSharp7Samples/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp7Samples
+{
+    public class Person
+    {
+        private string _firstName;
+        private string _lastName;
+        public Person(string name) => name.Split(' ').MoveElementsTo(out _firstName, out _lastName);
+
+        public string FirstName => _firstName;
+        public string LastName => _lastName;
+
+        private int _age;
+
+        public int Age
+        {
+            get => _age;
+            set => _age = value;
+        }
+
+        public void Deconstruct(out string firstname, out string lastname, out int age)
+        {
+            firstname = _firstName;
+            lastname = _lastName;
+            age = _age;
+        }
+    }
+
+    public static class StringCollectionExtension
+    {
+        public static void MoveElementsTo(this IList<string> coll, out string s1, out string s2)
+        {
+            if (coll.Count != 2) throw new ArgumentException("wrong collection count", nameof(coll));
+
+            s1 = coll[0];
+            s2 = coll[1];
+        }
+    }
+}
